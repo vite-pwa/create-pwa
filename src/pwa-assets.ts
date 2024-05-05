3113164b7f3004397b089ed094095b7a5b7b86c7
userquin <userquin@gmail.com>
2024-05-05 13:14:06 +0200

    chore: fix pwa assets

--- src/pwa-assets.ts
+++ src/pwa-assets.ts
@@ -3,13 +3,13 @@ import { addPackageObject } from './utils'
 export function includePwaAssets(npmPM: boolean, pkg: any) {
   addPackageObject(
     'scripts',
-    ['generate-pwa-icons', 'pwa-assets-generator'],
+    [['generate-pwa-icons', 'pwa-assets-generator']],
     pkg,
     false,
   )
   addPackageObject(
     'devDependencies',
-    ['@vite-pwa/assets-generator', '^0.2.4'],
+    [['@vite-pwa/assets-generator', '^0.2.4']],
     pkg,
   )
   addPackageObject(
@@ -17,7 +17,4 @@ export function includePwaAssets(npmPM: boolean, pkg: any) {
     [['sharp', '0.32.6'], ['sharp-ico', '0.1.5']],
     pkg,
   )
-}
-
-export function generatePwaAssets() {
 }
