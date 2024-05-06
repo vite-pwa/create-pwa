c7ee153aaecf5cf392788ec709594c5d1cd50b82
userquin <userquin@gmail.com>
2024-05-06 22:21:35 +0200

    chore: add customizations

--- templates/template-vue-ts/src/App.vue
+++ templates/template-vue-ts/src/App.vue
@@ -6,7 +6,7 @@ import PWABadge from './components/PWABadge.vue'
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/favicon.svg" class="logo" alt="Vite logo" />
+      <img src="/favicon.svg" class="logo" alt="Vite PWA + Vue logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
