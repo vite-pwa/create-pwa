db900d2fbe052ffe202b4ecde69377d716f9de69
userquin <userquin@gmail.com>
2024-05-07 19:28:19 +0200

    feat: add preact templates

--- src/customize/preact.ts
+++ src/customize/preact.ts
@@ -1,5 +1,124 @@
+import { renameSync, rmSync } from 'node:fs'
+import { generateCode, parseModule } from 'magicast'
+import { addVitePlugin } from 'magicast/helpers'
 import type { PromptsData } from '../types'
+import { editFile } from '../utils'
+import { preparePWAOptions } from '../pwa'
+import { MagicastViteOptions } from '../vite'
 
-export function customize(_prompts: PromptsData) {
+export function customize(prompts: PromptsData) {
+  const {
+    rootPath,
+    name,
+    themeColor,
+    framework,
+    customServiceWorker,
+    prompt,
+    offline,
+    reloadSW,
+    pwaAssets,
+  } = prompts
+  const ts = framework === 'preact-ts'
+  editFile(`${rootPath}/src/PWABadge.${ts ? 't' : 'j'}sx`, (content) => {
+    let newContent = content
 
+    if (!offline) {
+      newContent = newContent
+        .replace(
+          'offlineReady: [offlineReady, setOfflineReady],',
+          '',
+        )
+        .replace(
+          'setOfflineReady(false)',
+          '',
+        )
+        .replace(
+          '{ (offlineReady || needRefresh)',
+          '{ (needRefresh)',
+        )
+        .replace('{ offlineReady', '<span id="toast-message">New content available, click on reload button to update.</span>')
+        .replace('? <span id="toast-message">App ready to work offline</span>', '')
+        .replace(': <span id="toast-message">New content available, click on reload button to update.</span>}', '')
+        .replace(
+          '{ needRefresh && <button className="PWABadge-toast-button" onClick={() => updateServiceWorker(true)}>Reload</button> }',
+          '<button className="PWABadge-toast-button" onClick={() => updateServiceWorker(true)}>Reload</button>',
+        )
+    }
+
+    if (reloadSW) {
+      newContent = newContent
+        .replace('PERIODIC_SYNC_COMMENT', 'check for updates every hour')
+        .replace('const period = 0', 'const period = 60 * 60 * 1000')
+    }
+    else {
+      newContent = newContent.replace('PERIODIC_SYNC_COMMENT', `periodic sync is disabled, change the value to enable it, the period is in milliseconds
+// You can remove onRegisteredSW callback and registerPeriodicSync function`)
+    }
+
+    return newContent
+  })
+  editFile(`${rootPath}/index.html`, (content) => {
+    const newContent = content.replace('Vite PWA + Preact', `${name}`)
+    return pwaAssets
+      ? newContent.replace('<link rel="icon" type="image/svg+xml" href="/favicon.svg" />', '')
+      : newContent
+        .replace(
+          '<link rel="icon" type="image/svg+xml" href="/favicon.svg" />',
+                    `    <meta name="theme-color" content="${themeColor}">
+    <link rel="icon" href="/favicon.ico" sizes="48x48">
+    <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png">
+`,
+        )
+  })
+  editFile(`${rootPath}/src/app.${ts ? 't' : 'j'}sx`, (content) => {
+    return content
+      .replace('App logo', `${name} logo`)
+      .replace('<h1>Vite PWA + Preact</h1>', `<h1>${name}</h1>`)
+  })
+  if (customServiceWorker) {
+    const toDelete = prompt ? 'claims' : 'prompt'
+    rmSync(`${rootPath}/src/${toDelete}-sw.${ts ? 't' : 'j'}s`)
+    const toRename = prompt ? 'prompt' : 'claims'
+    renameSync(`${rootPath}/src/${toRename}-sw.${ts ? 't' : 'j'}s`, `${rootPath}/src/sw.${ts ? 't' : 'j'}s`)
+  }
+  else {
+    rmSync(`${prompts.rootPath}/src/prompt-sw.${ts ? 't' : 'j'}s`, { recursive: true })
+    rmSync(`${prompts.rootPath}/src/claims-sw.${ts ? 't' : 'j'}s`, { recursive: true })
+  }
+  const viteConf = parseModule(`
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    preact(),
+  ],
+})
+`)
+
+  addVitePlugin(viteConf, {
+    from: 'vite-plugin-pwa',
+    imported: 'VitePWA',
+    constructor: 'VitePWA',
+    options: preparePWAOptions(
+      ts,
+      prompts,
+      'src',
+      {
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+          cleanupOutdatedCaches: true,
+          clientsClaim: true,
+        },
+        injectManifest: {
+          globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        },
+      },
+    ),
+  })
+  editFile(`${rootPath}/vite.config.${ts ? 't' : 'j'}s`, () => {
+    return generateCode(viteConf, MagicastViteOptions).code
+  })
 }
--- templates/template-preact-ts/vite.config.ts
+++ templates/template-preact-ts/vite.config.ts
@@ -1,7 +1,63 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+      preact(),
+    VitePWA({
+      strategies: 'generateSW',
+      // when using strategies 'injectManifest' you need to provide the srcDir
+      // srcDir: 'src',
+      // when using strategies 'injectManifest' use claims-sw.ts or prompt-sw.ts
+      // filename: 'prompt-sw.ts',
+      registerType: 'prompt',
+      injectRegister: false,
+      pwaAssets: { disabled: false, config: true, htmlPreset: '2023', overrideManifestIcons: true },
+      manifest: {
+        name: 'Vite PWA + React + TS',
+        short_name: 'Vite PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,svg,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+      injectManifest: {
+        globPatterns: ['**/*.{js,css,html,svg,png,svg,ico}'],
+      },
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        /* when using generateSW the PWA plugin will switch to classic */
+        type: 'module',
+      },
+    })
+  ],
 })
