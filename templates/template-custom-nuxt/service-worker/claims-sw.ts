00f0317159114365ba3bb9fc2d6b65abfdba0b17
userquin <userquin@gmail.com>
2025-10-14 19:17:21 +0200

    chore: add payload `urlManipulation` to nuxt custom sw

--- templates/template-custom-nuxt/service-worker/claims-sw.ts
+++ templates/template-custom-nuxt/service-worker/claims-sw.ts
@@ -6,7 +6,20 @@ import { NavigationRoute, registerRoute } from 'workbox-routing'
 declare let self: ServiceWorkerGlobalScope
 
 // self.__WB_MANIFEST is the default injection point
-precacheAndRoute(self.__WB_MANIFEST)
+precacheAndRoute(
+  self.__WB_MANIFEST,
+  {
+    urlManipulation: ({ url }) => {
+      const urls: URL[] = []
+      if (url.pathname.endsWith('_payload.json')) {
+        const newUrl = new URL(url.href)
+        newUrl.search = ''
+        urls.push(newUrl)
+      }
+      return urls
+    }
+  }
+)
 
 // clean old assets
 cleanupOutdatedCaches()
